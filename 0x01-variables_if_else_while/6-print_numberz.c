c64b43ea4692827e8dea64343d02aff9e9c0a8f9
G3rt92 <beukesbg@gmail.com>
2022-09-09 13:04:16 -0700

    Variables and control structures

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57; i++)
+	{
+		putchar (i);
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
