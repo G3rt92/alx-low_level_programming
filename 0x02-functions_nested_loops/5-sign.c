ef2fc21c0ccf920f8851ca9bb8bddab3e759a2ca
G3rt92 <beukesbg@gmail.com>
2022-09-13 20:58:25 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- *  print_last_digit - last digit
+ *  print_sign - print sign
  *
  *@n: The int to print
  * Return: 0 for success.
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
