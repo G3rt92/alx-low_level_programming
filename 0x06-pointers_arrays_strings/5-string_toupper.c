544c15aa5d08904296e15c924a22c5923dd5506a
G3rt92 <beukesbg@gmail.com>
2022-09-22 14:00:10 -0700

    turn to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * string_toupper - changes all string letters from lowercase to an uppercase
+ * @str: string pointer args
+ * Return: char pointer type
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+
+	return (str);
+}
