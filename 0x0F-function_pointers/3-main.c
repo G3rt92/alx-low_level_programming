100c5dad54f8504b64a3343d544cd5989ea948ad
G3rt92 <beukesbg@gmail.com>
2022-10-12 22:30:10 +0200

    simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "3-calc.h".
+#include "3-calc.h"
 /**
  * main - program that perfroms simple operations
  * @argc: number of arguments
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	o = *argv[2];
+	c = *argv[2];
 
 	if ((c == '/' || c == '%') && (arg2 == 0))
 	{
