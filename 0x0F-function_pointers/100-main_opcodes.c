2bb8deeaae9592db8936a564116716d387e6d061
G3rt92 <beukesbg@gmail.com>
2022-10-12 22:33:10 +0200

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - prints its own opcodes
  * @argc: number of arguments
  * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * Return: 0 if Success
  */
 int main(int argc, char *argv[])
 {
