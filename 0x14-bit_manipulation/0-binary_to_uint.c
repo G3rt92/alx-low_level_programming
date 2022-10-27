f73c640076cba078cd2a1bc0b6dd2fd6e7ea21c3
G3rt92 <beukesbg@gmail.com>
2022-10-27 23:59:51 +0200

    bin to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,62 @@
 #include "main.h"
 /**
+ * _atoi - convert a string to an integer
+ *@s: String
+ * Return: Return the num
+ **/
+int _atoi(char *s)
+{
+	unsigned int num, i;
+	int sign;
+
+	sign = 1;
+	num = 0;
+
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = (s[i] - 48) + num * 10;
+
+			if (s[i + 1] == ' ')
+				break;
+		}
+		else if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+
+	}
+
+	return (num *sign);
+}
+/**
  * binary_to_uint - converts a string of 1's and 0's to a decimal number
  * @b: string to convert
  * Return: unsigned decimal number
  **/
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int num, bin_num, dec_num, base, rem;
-    int i;
-    dec_num = 0;
-    base = 1;
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] == 'e')
-            return (0);
-    }
-    num = atoi(b);
-    bin_num = num;
+	unsigned int num, bin_num, dec_num, base, rem;
+	int i;
+	dec_num = 0;
+	base = 1;
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == 'e')
+			return (0);
+	}
+	num = atoi(b);
+	bin_num = num;
 
-    while (num > 0)
-    {
-        rem = num % 10;
-        dec_num = dec_num + rem * base;
-        base = base * 2;
-        num = num / 10;
-    }
-    return ((int)dec_num);
+	while (num > 0)
+	{
+		rem = num % 10;
+		dec_num = dec_num + rem * base;
+		base = base * 2;
+		num = num / 10;
+	}
+	return ((int)dec_num);
 }
